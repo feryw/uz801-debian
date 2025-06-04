d40969740e0ce637b86af6bd732a523c34f74e76
Haris131 <41672454+Haris131@users.noreply.github.com>
2025-06-04 12:42:40 +0700

    Update chroot.sh

--- rootfs/chroot.sh
+++ rootfs/chroot.sh
@@ -26,9 +26,7 @@ EOF
 
 apt-get update
 apt-get full-upgrade -y
-apt-get install -y locales network-manager openssh-server systemd-timesyncd fake-hwclock zram-tools rmtfs qrtr-tools sudo
-apt-get install -y libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git
-apt-get install -y libllvm-16-ocaml-dev libllvm16 llvm-16 llvm-16-dev llvm-16-doc llvm-16-examples llvm-16-runtime clang-16 clang-tools-16 clang-16-doc libclang-common-16-dev libclang-16-dev libclang1-16 clang-format-16 python3-clang-16 clangd-16 clang-tidy-16 libclang-rt-16-dev libpolly-16-dev libfuzzer-16-dev lldb-16 lld-16 libc++-16-dev libc++abi-16-dev libomp-16-dev libclc-16-dev libunwind-16-dev libmlir-16-dev mlir-16-tools flang-16 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64
+apt-get install -y locales network-manager openssh-server systemd-timesyncd fake-hwclock zram-tools rmtfs qrtr-tools sudo curl wget neofetch
 apt-get install -y /tmp/*.deb
 ln -sf /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 timedatectl set-ntp 1
@@ -73,13 +71,6 @@ cat <<EOF >> /home/chewy/.bashrc
 clear
 /usr/bin/neofetch
 EOF
-
-git clone https://github.com/Oink70/CCminer-ARM-optimized.git
-cd CCminer-ARM-optimized
-chmod +x build.sh
-chmod +x configure.sh
-chmod +x autogen.sh
-CXX=clang++ CC=clang ./build.sh
 
 echo "## Default Configuration" > /tmp/info.md
 vmlinuz_name=$(basename /boot/vmlinuz-*)
